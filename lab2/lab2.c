c2de535d528fddefdd254bffbe6c0da108b7ed47
José Costa <up201907216@fe.up.pt>
2020-10-10 18:22:53 +0100

    Simplified error conditions

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -31,16 +31,15 @@ int main(int argc, char *argv[]) {
 
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-    uint8_t *st;
+    uint8_t st;
 
-    if (timer_get_conf(timer, st) != 0)
+    if (timer_get_conf(timer, &st))
         return 1;
 
-    if(timer_display_conf(timer, st, field) != 0)
+    if(timer_display_conf(timer, st, field))
         return 1;
         
     return 0;
-    printf("%s \n", _func_);
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
