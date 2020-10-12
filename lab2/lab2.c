6379e0700d175b13efb356a64b38fd613e375fd7
José Costa <up201907216@fe.up.pt>
2020-10-12 11:04:41 +0100

    Added timer_test_time_base

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -43,15 +43,11 @@ int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
 
-  timer_set_frequency(timer, freq);
+  if(timer_set_frequency(timer, freq))
+    return 1;
 
-  // CONTINUE ....
-
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  return 0;
 }
 
 int(timer_test_int)(uint8_t time) {
