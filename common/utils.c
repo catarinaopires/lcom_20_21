485c1f731f762ad02bd132fdaf36c9c9496831ae
José Costa <up201907216@fe.up.pt>
2020-10-24 17:37:52 +0100

    i8042.h file edited, added useful macros

--- common/utils.c
+++ common/utils.c
@@ -22,8 +22,10 @@ int (util_sys_inb)(int port, uint8_t *value) {
   unsigned int value32;
 
   //Calls minix SYS_DEVIO to read from the specified port
-  if(sys_inb(port, &value32) != OK)
-    return 1;
+  if(sys_inb(port, &value32) != OK) {
+      printf("Error in %s function's sys_inb call", __func__)
+      return 1;
+  }
 
   //Derefrences the value pointer, changing the memory content to the value read with the SYS_DEVIO call
   *value = value32;
