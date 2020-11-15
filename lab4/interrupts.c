4fc5eb96224aa153698129cf4e549150531c6e77
José Costa <up201907216@fe.up.pt>
2020-11-15 16:13:26 +0000

    lab4 mouse_async

--- lab4/interrupts.c
+++ lab4/interrupts.c
@@ -3,22 +3,30 @@
 #include <stdint.h>
 
 // Using int global variable to obey minix SYS_IRQCTL calls
-int HOOK_ID;
+int HOOK_ID[2];
+int HOOK_ID_COUNTER = 0;
 
-int(subscribe_int)(int irq, int policy, uint8_t *bit_no) {
-  HOOK_ID = *bit_no;
 
-  if (sys_irqsetpolicy(irq, policy, &HOOK_ID) != OK) {
+int (subscribe_int)(int irq, int policy, uint8_t *bit_no){
+  HOOK_ID[HOOK_ID_COUNTER] = *bit_no;
+
+  if(sys_irqsetpolicy(irq, policy, &HOOK_ID[HOOK_ID_COUNTER]) != OK){
     printf("Error in %s!", __func__);
     return 1;
   }
+
+  HOOK_ID_COUNTER++;
   return 0;
 }
 
-int unsubscribe_int() {
-  if (sys_irqrmpolicy(&HOOK_ID) != OK) {
-    printf("Error in %s!", __func__);
-    return 1;
+
+int (unsubscribe_int)(){
+
+  for(int i = 0; i < HOOK_ID_COUNTER; i++){
+    if(sys_irqrmpolicy(&HOOK_ID[i]) != OK){
+      printf("Error in %s!", __func__);
+      return 1;
+    }
   }
   return 0;
 }
--- lab4/lab4.c
+++ lab4/lab4.c
@@ -3,12 +3,17 @@
 
 #include "i8042.h"
 #include "interrupts.h"
+#include "i8254.h"
+#include "timer.h"
 #include <stdint.h>
 #include <stdio.h>
 
 #ifndef IRQ_SET
 #  define IRQ_SET 12
 #endif
+
+int TIMER_COUNTER = 0;
+
 // Any header files included below this line should have been created by you
 
 extern uint8_t OUTPUT_BUFF_DATA;
@@ -95,9 +100,67 @@ int (mouse_test_packet)(uint32_t cnt) {
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
-    /* To be completed */
-    printf("%s(%u): under construction\n", __func__, idle_time);
+  int ipc_status;
+  int r;
+  message msg;
+  uint8_t irq_set = IRQ_SET;
+  uint8_t timer_irq_set = TIMER0_IRQ;
+  uint8_t counter = 0;
+  uint8_t bytes[3] = {0, 0, 0};
+
+  if(mouse_enable_data_reporting_ours())
     return 1;
+
+  if(subscribe_int(KBC_MOUSE_IRQ, (IRQ_REENABLE | IRQ_EXCLUSIVE), &irq_set))
+    return 1;
+
+  if(subscribe_int(TIMER0_IRQ, IRQ_REENABLE, &timer_irq_set))
+    return 1;
+
+  while ((TIMER_COUNTER/60) < idle_time) {
+    /* Get a request message. */
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:                             /* hardware interrupt notification */
+          if (msg.m_notify.interrupts & BIT(irq_set)) { /* subscribed interrupt */
+            TIMER_COUNTER = 0;
+            mouse_ih();
+            bytes[counter] = OUTPUT_BUFF_DATA;
+            if (counter == 2) {
+              counter = 0;
+              struct packet toPrint = mouse_process_packet(bytes);
+              mouse_print_packet(&toPrint);
+            }
+            else if (counter == 1 || counter == 0) {
+              counter++;
+            }
+
+          }
+
+          if(msg.m_notify.interrupts & BIT(timer_irq_set)){
+            timer_int_handler();
+          }
+          break;
+        default:
+          break; /* no other notifications expected: do nothing */
+      }
+    }
+    else { /* received a standard message, not a notification */
+      /* no standard messages expected: do nothing */
+    }
+  }
+
+  if(unsubscribe_int())
+    return 1;
+
+  if(mouse_disable_data_reporting() != 0)
+    return 1;
+
+  return 0;
 }
 
 int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
