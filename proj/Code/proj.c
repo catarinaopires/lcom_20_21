2a6c3fcf444428d020bcbb7cc8987c76c6327c1a
Catarina Pires <up201907925@fe.up.pt>
2020-12-14 00:07:05 +0000

    Created function w/ module 'Jogo Reacao' & resized bombs

--- proj/Code/proj.c
+++ proj/Code/proj.c
@@ -11,6 +11,7 @@
 #include "video/images/bomb1.xpm"
 #include "video/images/bomb.xpm"
 #include "video/images/player_green.xpm"
+#include "video/images/background3.xpm"
 #include "kbc/i8042.h"
 #include "timer/i8254.h"
 #include "common/interrupts.h"
@@ -49,11 +50,170 @@ static int print_usage() {
 }
 */
 
+
+int jogo_reaçao(void){
+    video_instance instance = video_init_empty();
+    instance.mode = 0x14C;
+    instance.video_get_mode_info = video_get_mode_info;
+    instance.video_change_mode = video_change_mode;
+    instance.video_map_vram_mem = video_map_vram_mem;
+    instance.video_flip_page = NULL;
+    instance.video_get_current_buffer = video_get_current_buffer;
+    video_get_mode_info(&instance);
+    instance.bytesPerPixel = instance.mode_info.BitsPerPixel / 8;
+    video_map_vram_mem(&instance, 1);
+    video_change_mode(&instance, MODE_1152x864);
+
+    /*Image i = image_construct(background3_xpm, XPM_8_8_8_8,0,0);
+    image_draw(&i, &instance);*/
+
+    Sprite* sprite = create_sprite(player_green_xpm, 0,755, 0, 0);
+    image_draw(&sprite->drawing, &instance);
+
+
+    Sprite* sprite1 = create_sprite(bomb1_xpm, 350, 0, 0, 1);
+    Sprite* sprite2 = create_sprite(bomb_xpm, 700, 0, 0, 1);
+
+    Sprite* arr[] = {sprite, sprite1, sprite2};
+
+
+    int ipc_status;
+    int r;
+    message msg;
+    uint8_t irq_set_kbc = KBC_IRQ;
+    uint8_t irq_set_timer = TIMER0_IRQ;
+    uint8_t counter = 0;
+    uint8_t bytes[2] = {0, 0};
+
+    uint8_t keys[2] = {0, 0};
+    static direction d = none;
+    int collision = 0;
+    int counter_sec = 0;
+
+    // Subscribe keyboard interruptions
+    if (subscribe_int(KBC_IRQ, (IRQ_REENABLE | IRQ_EXCLUSIVE), &irq_set_kbc))
+        return 1;
+
+    // Subscribe timer interruptions
+    if (subscribe_int(TIMER0_IRQ, IRQ_REENABLE, &irq_set_timer))
+        return 1;
+
+    while (!collision) {
+        /* Get a request message. */
+        if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+            printf("driver_receive failed with: %d", r);
+            continue;
+        }
+        if (is_ipc_notify(ipc_status)) { /* received notification */
+            switch (_ENDPOINT_P(msg.m_source)) {
+                case HARDWARE:                             /* hardware interrupt notification */
+                    if (msg.m_notify.interrupts & BIT(irq_set_kbc)) {
+                        kbc_ih();
+                        bytes[counter] = OUTPUT_BUFF_DATA;
+
+                        if (counter == 1) {
+                            counter = 0;
+
+                            collision = check_collisions_sprite(arr, 3);
+                            if (!collision) {
+                                check_movement_r_l(&bytes[0], &d, &keys[0]);
+                                switch (d) {
+                                    case right:
+                                        change_speed(sprite, 5, 0);
+                                        move_sprite(sprite, 1152, 0, &instance);
+                                        break;
+                                    case left:
+                                        change_speed(sprite, -5, 0);
+                                        move_sprite(sprite, 0, 0, &instance);
+                                        break;
+                                    default:
+                                        change_speed(sprite, 0, 0);
+                                        move_sprite(sprite, 1152, 863 - sprite->drawing.img.width, &instance);
+                                        break;
+                                }
+                            }
+                        } else {
+                            if (OUTPUT_BUFF_DATA == KBC_SCANCODE_LEN_2)
+                                counter++;
+                            else {
+                                collision = check_collisions_sprite(arr, 3);
+                                if (!collision) {
+                                    check_movement_r_l(&bytes[0], &d, &keys[0]);
+
+                                    switch (d) {
+                                        case right:
+                                            change_speed(sprite, 5, 0);
+                                            move_sprite(sprite, 1152, 0, &instance);
+                                            break;
+                                        case left:
+                                            change_speed(sprite, -5, 0);
+                                            move_sprite(sprite, 0, 0, &instance);
+                                            break;
+                                        default:
+                                            change_speed(sprite, 0, 0);
+                                            move_sprite(sprite, 1152, 863 - sprite->drawing.img.width, &instance);
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (msg.m_notify.interrupts & BIT(irq_set_timer)) { /* subscribed interrupt */
+                        counter_sec++;
+
+                        collision = check_collisions_sprite(arr, 3);
+                        if (!collision) {
+                            if (move_sprite(sprite1, 350, 863, &instance) != 0) {
+                                draw_rectangle(350, 863 - sprite1->drawing.img.height, sprite1->drawing.img.width,
+                                               sprite1->drawing.img.height, xpm_transparency_color(XPM_8_8_8_8),
+                                               &instance);
+                                sprite1->drawing.x = 350;
+                                sprite1->drawing.y = 0;
+                            }
+                        }
+                        if (counter_sec >= 3 * 60) {
+                            collision = check_collisions_sprite(arr, 3);
+                            if (!collision) {
+                                if (move_sprite(sprite2, 700, 863, &instance) != 0) {
+                                    draw_rectangle(700, 863 - sprite2->drawing.img.height, sprite2->drawing.img.width,
+                                                   sprite2->drawing.img.height, xpm_transparency_color(XPM_8_8_8_8),
+                                                   &instance);
+                                    sprite2->drawing.x = 700;
+                                    sprite2->drawing.y = 0;
+                                }
+                            }
+                        }
+                    }
+
+                    break;
+                default:
+                    break; /* no other notifications expected: do nothing */
+            }
+
+        } else { /* received a standard message, not a notification */
+            /* no standard messages expected: do nothing */
+        }
+    }
+
+    // Unsubscribe both interruptions
+    if (unsubscribe_int())
+        return 1;
+
+    destroy_sprite(sprite1);
+    destroy_sprite(sprite2);
+    destroy_sprite(sprite);
+
+
+    instance.video_change_mode(&instance, MODE_TEXT);
+
+    return 0;
+}
+
+
 int(proj_main_loop)(int argc, char *argv[]) {
   /* 
   Substitute the code below by your own
-   
-
   //
   // if you're interested, try to extend the command line options so that the usage becomes:
   // <mode - hex> <minix3 logo  - true|false> <grayscale - true|false> <delay (secs)>
@@ -75,288 +235,5 @@ int(proj_main_loop)(int argc, char *argv[]) {
 
   //return proj_demo(mode, minix3_logo, grayscale, delay);
 
-  video_instance instance = video_init_empty();
-  instance.mode = 0x14C;
-  instance.video_get_mode_info = video_get_mode_info;
-  instance.video_change_mode = video_change_mode;
-  instance.video_map_vram_mem = video_map_vram_mem;
-  instance.video_flip_page = NULL;
-  instance.video_get_current_buffer = video_get_current_buffer; 
-  video_get_mode_info(&instance);
-  instance.bytesPerPixel = instance.mode_info.BitsPerPixel / 8;
-  video_map_vram_mem(&instance, 1);
-  video_change_mode(&instance, MODE_1152x864);
-
-   //draw_rectangle(100,100,100,100, (ENG_RED|ENG_GREEN|ENG_BLUE), &instance);
-  
-  // xpm_image_t ball;
-  // xpm_load(ball_xpm, XPM_8_8_8_8, &ball);
-  // draw_xpm(150, 20, &ball, &instance);
-
-/* Image i = image_construct(ball_xpm, XPM_8_8_8_8,0,0);
-  printf("draw: %d\n", image_draw(&i, &instance));
-  sleep(2);*/
-
-
-  Sprite* sprite = create_sprite(player_green_xpm, 0,755, 0, 0);
-  image_draw(&sprite->drawing, &instance);
-  sleep(2);
-
-
-  Sprite* sprite1 = create_sprite(bomb1_xpm, 350, 0, 0, 1);
-  Sprite* sprite2 = create_sprite(bomb_xpm, 700, 0, 0, 1);
-
-  Sprite* arr[] = {sprite, sprite1, sprite2};
-
-/*
-  int status =0;
-  int st = 0;
-  while (st != 1 && status != 1)
-  {
-    if(check_collisions_sprite(arr)){
-      printf("collision2\n");
-      break;
-    }
-    else{
-      st = move_sprite(sprite1,12,0, &instance);
-    }
-
-    if(check_collisions_sprite(arr)){
-      printf("collision1\n");
-      break;
-    }
-    else{
-      status = move_sprite(sprite,500,0, &instance);
-    }
-
-    if(status == 1){
-      printf("movement finished\n");
-      if(st == 1)
-        break;
-    }
-
-    if(st == 1)
-      printf("movement finished2\n");
-      if(status ==1)
-        break;
-
-  }*/
-
-
-    // TEST KBD DIRECTIONS
-    /*int ipc_status;
-    int r;
-    message msg;
-    uint8_t irq_set = KBC_IRQ;
-    uint8_t counter = 0;
-    uint8_t bytes[2] = {0, 0};
-    static direction d = none;
-
-    if(subscribe_int(KBC_IRQ, (IRQ_REENABLE | IRQ_EXCLUSIVE), &irq_set))
-        return 1;
-
-    while (bytes[0] != KBC_ESC_BREAKCODE) {
-        if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-            printf("driver_receive failed with: %d", r);
-            continue;
-        }
-        if (is_ipc_notify(ipc_status)) {
-            switch (_ENDPOINT_P(msg.m_source)) {
-                case HARDWARE:
-                    if (msg.m_notify.interrupts & BIT(irq_set)) {
-                        kbc_ih();
-                        bytes[counter] = OUTPUT_BUFF_DATA;
-                        if (counter == 1) {
-                            counter = 0;
-
-                            if(check_collisions_sprite(arr)){
-                                printf("COLLISION\n");
-                                break;
-                            }
-                            check_movement_r_l(&bytes[0], &d, &keys[0]);
-                            switch (d) {
-                                case right:
-                                    change_speed(sprite, 5, 0);
-                                    move_sprite(sprite, 1152, 0, &instance);
-                                    break;
-                                case left:
-                                    change_speed(sprite, -5, 0);
-                                    move_sprite(sprite, 0, 0, &instance);
-                                    break;
-                                default:
-                                    change_speed(sprite, 0, 0);
-                                    move_sprite(sprite, 1152, 863-sprite->drawing.img.width, &instance);
-                                    break;
-                            }
-
-                        }
-                        else {
-                            if (OUTPUT_BUFF_DATA == KBC_SCANCODE_LEN_2)
-                                counter++;
-                            else {
-
-                                check_movement_r_l(&bytes[0], &d, &keys[0]);
-                                switch (d) {
-                                    case right:
-                                        change_speed(sprite, 5, 0);
-                                        move_sprite(sprite, 1152, 0, &instance);
-                                        break;
-                                    case left:
-                                        change_speed(sprite, -5, 0);
-                                        move_sprite(sprite, 0, 0, &instance);
-                                        break;
-                                    default:
-                                        change_speed(sprite, 0, 0);
-                                        move_sprite(sprite, 1152, 863-sprite->drawing.img.width, &instance);
-                                        break;
-                                }
-                            }
-                        }
-                    }
-                    break;
-                default:
-                ;
-            }
-        }
-        else {
-        }
-    }
-    if(unsubscribe_int())
-        return 1;*/
-
-    //-------------------------------------------------------
-    int ipc_status;
-    int r;
-    message msg;
-    uint8_t irq_set_kbc = KBC_IRQ;
-    uint8_t irq_set_timer = TIMER0_IRQ;
-    uint8_t counter = 0;
-    uint8_t bytes[2] = {0, 0};
-
-    uint8_t keys[2] = {0,0};
-    static direction d = none;
-    int collision = 0;
-    int counter_sec = 0;
-
-    // Subscribe keyboard interruptions
-    if(subscribe_int(KBC_IRQ, (IRQ_REENABLE | IRQ_EXCLUSIVE), &irq_set_kbc))
-        return 1;
-
-    // Subscribe timer interruptions
-    if(subscribe_int(TIMER0_IRQ, IRQ_REENABLE, &irq_set_timer))
-        return 1;
-
-    while (!collision) {
-        /* Get a request message. */
-        if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-            printf("driver_receive failed with: %d", r);
-            continue;
-        }
-        if (is_ipc_notify(ipc_status)) { /* received notification */
-            switch (_ENDPOINT_P(msg.m_source)) {
-                case HARDWARE:                             /* hardware interrupt notification */
-                    if (msg.m_notify.interrupts & BIT(irq_set_kbc)) {
-                        kbc_ih();
-                        bytes[counter] = OUTPUT_BUFF_DATA;
-
-                        if (counter == 1) {
-                            counter = 0;
-
-                            collision = check_collisions_sprite(arr,3);
-                            if(!collision){
-                                check_movement_r_l(&bytes[0], &d, &keys[0]);
-                                switch (d) {
-                                    case right:
-                                        change_speed(sprite, 5, 0);
-                                        move_sprite(sprite, 1152, 0, &instance);
-                                        break;
-                                    case left:
-                                        change_speed(sprite, -5, 0);
-                                        move_sprite(sprite, 0, 0, &instance);
-                                        break;
-                                    default:
-                                        change_speed(sprite, 0, 0);
-                                        move_sprite(sprite, 1152, 863 - sprite->drawing.img.width, &instance);
-                                        break;
-                                }
-                            }
-                        }
-                        else {
-                            if (OUTPUT_BUFF_DATA == KBC_SCANCODE_LEN_2)
-                                counter++;
-                            else {
-                                collision = check_collisions_sprite(arr,3);
-                                if(!collision){
-                                    check_movement_r_l(&bytes[0], &d, &keys[0]);
-
-                                    switch (d) {
-                                        case right:
-                                            change_speed(sprite, 5, 0);
-                                            move_sprite(sprite, 1152, 0, &instance);
-                                            break;
-                                        case left:
-                                            change_speed(sprite, -5, 0);
-                                            move_sprite(sprite, 0, 0, &instance);
-                                            break;
-                                        default:
-                                            change_speed(sprite, 0, 0);
-                                            move_sprite(sprite, 1152, 863-sprite->drawing.img.width, &instance);
-                                            break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    if (msg.m_notify.interrupts & BIT(irq_set_timer)) { /* subscribed interrupt */
-                        counter_sec++;
-
-                        collision = check_collisions_sprite(arr, 3);
-                        if(!collision) {
-                            if (move_sprite(sprite1, 350, 863, &instance) != 0) {
-                                draw_rectangle(350, 863 - sprite1->drawing.img.height, sprite1->drawing.img.width,
-                                               sprite1->drawing.img.height, xpm_transparency_color(XPM_8_8_8_8),
-                                               &instance);
-                                sprite1->drawing.x = 350;
-                                sprite1->drawing.y = 0;
-                            }
-                        }
-                        if(counter_sec >= 3*60){
-                            collision = check_collisions_sprite(arr, 3);
-                            if(!collision) {
-                                if (move_sprite(sprite2, 700, 863, &instance) != 0) {
-                                    draw_rectangle(700, 863-sprite2->drawing.img.height, sprite2->drawing.img.width,
-                                                   sprite2->drawing.img.height, xpm_transparency_color(XPM_8_8_8_8),
-                                                   &instance);
-                                    sprite2->drawing.x = 700;
-                                    sprite2->drawing.y = 0;
-                                }
-                            }
-                        }
-                    }
-
-                    break;
-                default:
-                    break; /* no other notifications expected: do nothing */
-            }
-
-        }
-        else { /* received a standard message, not a notification */
-            /* no standard messages expected: do nothing */
-        }
-    }
-
-    // Unsubscribe both interruptions
-    if(unsubscribe_int())
-        return 1;
-
-    destroy_sprite(sprite1);
-    destroy_sprite(sprite2);
-    destroy_sprite(sprite);
-    sleep(2);
-
-
-  instance.video_change_mode(&instance, MODE_TEXT);
-  
-  return 0;
+    return jogo_reaçao();
 }
